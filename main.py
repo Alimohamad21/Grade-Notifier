85f759a2235d345f17d05bc017aa714a7607eb49
Ali Mohamed <alimohamadyoussef21@gmail.com>
2021-03-14 15:04:43 +0200

    new feature

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 
 from selenium import webdriver
 
+option = input('1- Coursework Notifier\n2- Final Grade Notifier\n\nPlease choose an option from the above:')
 data = json.load(open('D:\passwords.json'))
 password = data['site_password']
 username = data['site_username']
@@ -48,12 +49,50 @@ def midterm_marks_notifier():
                     pass
 
             if grade != updated_grades[course]:
-                updated_grades[course]=grade
+                updated_grades[course] = grade
                 root = tkinter.Tk()
                 root.withdraw()
                 messagebox.showinfo(course, grade)
-                json.dump(updated_grades,open('latest_results.json', 'w'))
+                json.dump(updated_grades, open('latest_results.json', 'w'))
         driver.refresh()
 
 
-midterm_marks_notifier()
+def final_grade_notifier():
+    while True:
+        while True:
+            try:
+                driver.find_element_by_xpath('/html/body/div/section[1]/nav/ul/li[8]/a').click()
+                if driver.find_element_by_xpath('/html/body/div/section[2]/div/div/h1').text == 'Courses Grades':
+                    break
+            except:
+                pass
+        updated_grades = json.load(open('final_grades.json', 'r'))
+        for i in range(1, 7):
+            while True:
+                try:
+                    course = driver.find_element_by_xpath(
+                        f"/html/body/div/section[2]/div/table[5]/tbody/tr[{i}]/td[2]").text
+                    break
+                except:
+                    pass
+            while True:
+                try:
+                    grade = driver.find_element_by_xpath(
+                        f"/html/body/div/section[2]/div/table[5]/tbody/tr[{i}]/td[4]").text
+                    break
+                except:
+                    pass
+
+            if grade != updated_grades[course]:
+                updated_grades[course] = grade
+                root = tkinter.Tk()
+                root.withdraw()
+                messagebox.showinfo(course, grade)
+                json.dump(updated_grades, open('final_grades.json', 'w'))
+        driver.refresh()
+
+
+if option == '1':
+    midterm_marks_notifier()
+if option == '2':
+    final_grade_notifier()
